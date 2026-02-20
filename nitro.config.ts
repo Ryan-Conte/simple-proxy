f44020af420f137eef283f3495a691646285b06d
Ryan Conte <ryan@conte.id>
2026-02-20 12:25:19 +1000

    lets hope it works

--- nitro.config.ts
+++ nitro.config.ts
@@ -1,24 +1,8 @@
-import { fileURLToPath } from 'url';
-import { dirname, join } from 'path';
-import { defineNitroConfig } from 'nitropack';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
 export default defineNitroConfig({
   noPublicDir: true,
   srcDir: './src',
-  preset: 'cloudflare_module',
-  rollupConfig: {
-    external: [
-      'node:events',
-      'node:perf_hooks',
-      'node:stream',
-      'node:tty',
-      '__STATIC_CONTENT_MANIFEST',
-    ],
-  },
+  preset: 'cloudflare',
   alias: {
-    '@': join(__dirname, 'src'),
+    '@': './src',
   },
 });
